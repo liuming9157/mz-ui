cb9b42384e2797b6072950e9ee24a0b32578a4f4
Jack Liu <liuming@mzsat.cn>
2020-02-03 18:55:20 +0800

    add avatar

---
+++ lib/mz-avatar/mz-avatar.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="">
+		<image class='avatar' :src='avatar' :style='{width:`${size}upx`,height:`${size}upx`}'></image>
+	</view>
+</template>
+
+<script>
+	export default{
+		name:'MzAvatar',
+		props:{
+			size:{
+				type:Number,
+				default:24
+			}
+		}
+	}
+</script>
+
+<style>
+	.avatar{
+		border: 50%;
+	}
+</style>
